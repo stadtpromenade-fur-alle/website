53214db6af0ef8021184bed1a3ab0c2c92399802
Fabian Große <saphareas@googlemail.com>
2019-08-01 19:06:37 +0200

    Fix Navbar

--- js/creative.js
+++ js/creative.js
@@ -35,32 +35,36 @@
         }
     );
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
-
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
 })(jQuery); // End of use strict
 
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+    transformNav();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("btn-top").style.display = "block";
-  } else {
-    document.getElementById("btn-top").style.display = "none";
-  }
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("btn-top").style.display = "block";
+    } else {
+        document.getElementById("btn-top").style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function transformNav() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.getElementById("mainNav").classList.add("affix");
+    } else {
+        document.getElementById("mainNav").classList.remove("affix");
+    }
 }
